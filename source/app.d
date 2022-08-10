aeccf097391c2c866829cb6bad6883ccf0a99b2a
Selim <sozel@wpi.edu>
2022-08-10 20:50:19 +0000

    Python script that sends mail

--- source/app.d
+++ source/app.d
@@ -1,12 +1,28 @@
-//module hello;
-
 import std.stdio;
 import pyd.pyd, pyd.embedded;
-/*
+
 shared static this() {
     py_init();
 }
-*/
+
 void main() {
-    //writeln(py_eval!string("'1 + %s' % 2"));
+    auto context = new InterpContext();
+    context.py_stmts("
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+
+        msg = MIMEMultipart()
+        msg.set_unixfrom('author')
+        msg['From'] = 'mail@server.com'
+        msg['To'] = 'mail@server.com'
+        msg['Subject'] = 'simple email in python'
+        message = 'here is the email'
+        msg.attach(MIMEText(message))
+
+        mailserver = smtplib.SMTP_SSL('smtpout.secureserver.net', 465)
+        mailserver.ehlo()
+        mailserver.login('mail@server.com', 'password')
+        mailserver.sendmail('mail@server.com','mail@server.com',msg.as_string())
+        mailserver.quit()");
 }

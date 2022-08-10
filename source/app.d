0e29802c6980dbff6d7f8893b46c9a8b2033f49d
Selim Ozel <sozel@wpi.edu>
2022-08-10 22:13:30 +0200

    Removed build residues

--- source/app.d
+++ source/app.d
@@ -1,6 +1,12 @@
-import std.stdio;
+//module hello;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import std.stdio;
+import pyd.pyd, pyd.embedded;
+/*
+shared static this() {
+    py_init();
+}
+*/
+void main() {
+    //writeln(py_eval!string("'1 + %s' % 2"));
 }
